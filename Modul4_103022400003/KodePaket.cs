7e9e88a64a6b8aec5099972820a17a7497c64b91
Your NameMoCheeseKy <rifkyking04@gmail.com>
2026-03-28 14:15:48 +0700

    second commit

---
+++ Modul4_103022400003/KodePaket.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul4_103022400003
+{
+    internal class KodePaket
+    {
+        public static string getKodePaket(string namaPaket)
+        {
+            string[] namaPaketList = {
+                "Basic", "Standard", "Premium", "Unlimited",
+                "Gaming", "Streaming", "Family", "Business",
+                "Student", "Traveler"
+            };
+
+            string[] kodePaketTable = {
+                "P201", "P202", "P203", "P204",
+                "P205", "P206", "P207", "P208",
+                "P209", "P210"
+            };
+
+            for (int i = 0; i < namaPaketList.Length; i++)
+            {
+                if (namaPaketList[i] == namaPaket)
+                    return kodePaketTable[i];
+            }
+
+            return "Paket tidak ditemukan";
+        }
+    }
+}
